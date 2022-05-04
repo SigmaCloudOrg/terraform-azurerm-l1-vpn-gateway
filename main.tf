72de0ea43db0a7baf9cfa987e138b8f0e8f3e045
James Ignatius <James906448@majesco.com>
2022-05-04 03:36:04 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,36 @@
 locals {
     prefix_input = var.name_prefix != "" ? var.name_prefix : var.deployment_name
-    name_prefix = "pubip-${local.prefix_input}"
+    name_prefix = "vpngw-${local.prefix_input}"
+    bgp_settings_block = {
+        asn             = var.bgp_settings_asn
+        peer_weight     = var.bgp_settings_peer_weight
+        custom_ips      = var.bgp_settings_custom_ips
+    }
 }
 
-resource "azurerm_public_ip" "generic" {
+resource "azurerm_vpn_gateway" "generic" {
   name                      = "${local.name_prefix}-${var.name}"
   resource_group_name       = var.resource_group_name
   location                  = var.location
-  allocation_method         = var.allocation_method
-  zones                     = var.zones
-  domain_name_label         = var.domain_name_label
-  edge_zone                 = var.edge_zone
-  idle_timeout_in_minutes   = var.idle_timeout_in_minutes
-  ip_tags                   = var.ip_tags
-  ip_version                = var.ip_version
-  public_ip_prefix_id       = var.public_ip_prefix_id
-  reverse_fqdn              = var.reverse_fqdn
-  sku                       = var.sku
-  sku_tier                  = var.sku_tier
+  virtual_hub_id            = var.virtual_hub_id
+  routing_preference        = var.routing_preference
+  scale_unit                = var.scale_unit
+  
+  dynamic "bgp_settings" {
+      for_each = var.bgp_settings_enabled ? local.bgp_settings_block : []
+      content {
+        asn                 = bgp_settings.value.asn
+        peer_weight         = bgp_settings.value.peer_weight
+        instance_bgp_peering_address {
+            custom_ips      = bgp_settings.value.custom_ips
+        }
+      }
+  }
   tags                      = merge(
                                 var.tags,
                                     tomap({
                                         "name" = "${local.name_prefix}-${var.name}",
-                                        "resource" = "azurerm_public_ip",
+                                        "resource" = "azurerm_vpn_gateway",
                                         "service" = "network",
                                         "dataclass" = "public"
                                     })
